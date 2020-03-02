57a766435e2cdeb3730250a170fb0526fa7b4060
alexdevyatov <alexeidevyatov94@gmail.com>
2020-03-03 02:26:22 +0400

    Поправил верстку

--- app/src/main/java/com/alexdevyatov/testtask/view/fragment/CompanyCardFragment.kt
+++ app/src/main/java/com/alexdevyatov/testtask/view/fragment/CompanyCardFragment.kt
@@ -2,6 +2,7 @@ package com.alexdevyatov.testtask.view.fragment
 
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -44,6 +45,7 @@ class CompanyCardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        tvCompanyDesc.movementMethod = ScrollingMovementMethod()
         companyInfoViewModel!!.companyId = args.companyId
     }
 
